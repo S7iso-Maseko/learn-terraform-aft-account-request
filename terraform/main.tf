b4ed1217f0ba6e8da84c9cf1936c4d5e04345056
S7iso <82435868+S7iso-Maseko@users.noreply.github.com>
2025-05-18 14:59:54 +0200

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,7 +4,7 @@ module "sandbox" {
   control_tower_parameters = {
     AccountEmail              = "smaseko+sandbox-aft@amazon.com"
     AccountName               = "sandbox-aft"
-    ManagedOrganizationalUnit = "Learn AFT"
+    ManagedOrganizationalUnit = "AFT OU"
     SSOUserEmail              = "smaseko+sandbox-aft@amazon.com"
     SSOUserFirstName          = "Sandbox"
     SSOUserLastName           = "AFT"
